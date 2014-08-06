ce7d64c8be16c225e8d7204b873555e956806a55
Jeff Patton <jeffrey@patton-tech.com>
2014-08-06 11:34:35 -0500

    Added the maxReceivedMessageSize property to app.config and rolled back the changes to passing in params, currently passing in the binding and endpoint results in unrecognized message error

--- ServiceNOW/incident.cs
+++ ServiceNOW/incident.cs
@@ -6,18 +6,18 @@
     public class Incident
     {
         /// <summary>
-        /// 
+        /// This function creates a new incident in ServiceNOW
         /// </summary>
-        /// <param name="Credential"></param>
-        /// <param name="Incident"></param>
-        /// <param name="EndPoint"></param>
-        /// <param name="RemoteAddress"></param>
-        /// <returns></returns>
-        public static insertResponse NewIncident(NetworkCredential Credential, insert Incident, string EndPoint, string RemoteAddress)
+        /// <param name="Credential">A credential object that gets passed to the soap client for authentication</param>
+        /// <param name="Incident">An insert object that contains the data to be inserted into ServiceNOW</param>
+        /// <returns>An insertResponse object containing data returned from ServiceNOW</returns>
+        public static insertResponse NewIncident(NetworkCredential Credential, insert Incident)
         {
             try
             {
-                ServiceNowSoapClient client = soapClient(Credential, EndPoint, RemoteAddress);
+                string userName = Credential.UserName;
+                string userPass = Credential.Password;
+                ServiceNowSoapClient client = soapClient(userName, userPass);
                 
                 insertResponse response = new insertResponse();
 
@@ -30,18 +30,18 @@
             }
         }
         /// <summary>
-        /// 
+        /// Get one or more incidents from ServiceNOW
         /// </summary>
-        /// <param name="Credential"></param>
-        /// <param name="Incident"></param>
-        /// <param name="EndPoint"></param>
-        /// <param name="RemoteAddress"></param>
-        /// <returns></returns>
-        public static getRecordsResponseGetRecordsResult[] GetIncident(NetworkCredential Credential, getRecords Incident, string EndPoint, string RemoteAddress)
+        /// <param name="Credential">A NetworkCredential object that gets passed to the soap client for authentication</param>
+        /// <param name="Incident">A getRecrods object that contains the data to be retreived from ServiceNOW</param>
+        /// <returns>A getRecordsResponseGetRecordsResult array of incidents</returns>
+        public static getRecordsResponseGetRecordsResult[] GetIncident(NetworkCredential Credential, getRecords Incident)
         {
             try
             {
-                ServiceNowSoapClient client = soapClient(Credential, EndPoint, RemoteAddress);
+                string userName = Credential.UserName;
+                string userPass = Credential.Password;
+                ServiceNowSoapClient client = soapClient(userName, userPass);
 
                 getRecordsResponseGetRecordsResult[] result = client.getRecords(Incident);
 
@@ -53,19 +53,18 @@
             }
         }
         /// <summary>
-        /// 
+        /// A private function to create the soapclient to communicate with ServiceNOW
         /// </summary>
-        /// <param name="Credential"></param>
-        /// <param name="EndPoint"></param>
-        /// <param name="RemoteAddress"></param>
-        /// <returns></returns>
-        private static ServiceNowSoapClient soapClient(NetworkCredential Credential, string EndPoint, string RemoteAddress)
+        /// <param name="UserName">The API Username</param>
+        /// <param name="Password">The API Password</param>
+        /// <returns>A ServiceNow SoapClient object</returns>
+        private static ServiceNowSoapClient soapClient(string UserName, string Password)
         {
             try
             {
-                ServiceNowSoapClient soapClient = new ServiceNowSoapClient(EndPoint, RemoteAddress);
-                soapClient.ClientCredentials.UserName.UserName = Credential.UserName;
-                soapClient.ClientCredentials.UserName.Password = Credential.Password;
+                ServiceNowSoapClient soapClient = new ServiceNowSoapClient();
+                soapClient.ClientCredentials.UserName.UserName = UserName;
+                soapClient.ClientCredentials.UserName.Password = Password;
                 return soapClient;
             }
             catch (Exception ex)
